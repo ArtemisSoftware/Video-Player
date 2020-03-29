3e57d35e8241e16bae332e0b52fec2e28785b3ad
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-03-29 20:53:10 +0100

    ExoplayerActivity

--- app/src/main/java/com/artemissoftware/videoplayer/ExoplayerActivity.java
+++ app/src/main/java/com/artemissoftware/videoplayer/ExoplayerActivity.java
@@ -1,14 +1,60 @@
 package com.artemissoftware.videoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.artemissoftware.videoplayer.models.MediaObject;
+import com.artemissoftware.videoplayer.util.Resources;
+import com.artemissoftware.videoplayer.util.VerticalSpacingItemDecorator;
+import com.artemissoftware.videoplayer.util.VideoPlayerRecyclerAdapter;
+import com.artemissoftware.videoplayer.util.VideoPlayerRecyclerView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ExoplayerActivity extends AppCompatActivity {
+
+
+    private VideoPlayerRecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exoplayer);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+
+        ArrayList<MediaObject> mediaObjects = new ArrayList<MediaObject>(Arrays.asList(Resources.MEDIA_OBJECTS));
+        mRecyclerView.setMediaObjects(mediaObjects);
+        VideoPlayerRecyclerAdapter adapter = new VideoPlayerRecyclerAdapter(mediaObjects, initGlide());
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private RequestManager initGlide(){
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background);
+
+        return Glide.with(this)
+                .setDefaultRequestOptions(options);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if(mRecyclerView!=null)
+            mRecyclerView.releasePlayer();
+        super.onDestroy();
     }
 }
