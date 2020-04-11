e5c9e9749a48e1df22556cf7da521bce4409f176
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-04-11 20:14:37 +0100

    Enable Android's full-screen mode

--- app/src/main/java/com/artemissoftware/videoplayer/LiveStreamingActivity.java
+++ app/src/main/java/com/artemissoftware/videoplayer/LiveStreamingActivity.java
@@ -3,6 +3,7 @@ package com.artemissoftware.videoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LiveStreamingActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class LiveStreamingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_streaming);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+        if (rootView != null)
+            rootView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
